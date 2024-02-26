b3a2d4ff0a924b8f43bf90275834190cc0a80ce8
Genius-code <moh.aly8890@gmail.com>
2024-02-26 13:50:34 +0200

    address_api

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class City extends Model
 {
-    use HasFactory;
+    use HasFactoryHasTranslations;
+
+    public $translatable = [
+        'name'
+    ];
+    protected $fillable = [
+        'name', 'country_id'
+    ];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function areas()
+    {
+        return $this->hasMany(Area::class);
+    }
 }
