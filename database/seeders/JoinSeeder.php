a3f576480065272b61592da0331cafefad896738
Genius-code <moh.aly8890@gmail.com>
2024-03-09 19:53:40 +0200

    change_lang_api

---
+++ database/seeders/JoinSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Invoice;
+use App\Models\Join;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JoinSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $invoice = Invoice::create([
+            'user_id'=>3,
+            'training_id'=>2,
+            'order_number'=>23,
+            'status'=>'paid',
+            'amount'=>123
+        ]);
+
+        Join::create([
+           'invoice_id'=>$invoice->id,
+           'training_id'=>$invoice->training_id,
+        ]);
+    }
+}
