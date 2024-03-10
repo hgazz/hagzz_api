1b63d18179c241d3e6e372eb1a6b8b6f05217c2d
Genius-code <moh.aly8890@gmail.com>
2024-03-10 14:14:44 +0200

    change_lang_api

--- app/Models/TClass.php
+++ app/Models/TClass.php
@@ -15,10 +15,10 @@ class TClass extends Model
 
     protected $hidden = ['created_at', 'updated_at','academy_id','sport_id','training_id'];
 
-    protected $casts = [
-        'out_comes' => 'array',
-        'bring_with_me' => 'array',
-    ];
+//    protected $casts = [
+//        'out_comes' => 'array',
+//        'bring_with_me' => 'array',
+//    ];
     public function academy()
     {
         return $this->belongsTo(Academies::class, 'academy_id', 'id');
@@ -47,13 +47,13 @@ class TClass extends Model
 
     public function getOutComesAttribute($value)
     {
-        return json_decode($value, true);
+        return json_decode($value);
 
     }
 
-//    public function getBringWithMeAttribute($value)
-//    {
-//        return json_decode($value, true);
-//    }
+    public function getBringWithMeAttribute($value)
+    {
+        return json_decode($value);
+    }
 
 }
