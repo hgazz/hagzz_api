b15d84abdcbbb7efae0bd422d172c5413f37496d
Genius-code <moh.aly8890@gmail.com>
2024-03-11 14:51:55 +0200

    update_explore_api

--- app/Http/Controllers/Api/TrainingController.php
+++ app/Http/Controllers/Api/TrainingController.php
@@ -15,7 +15,7 @@ class TrainingController extends Controller
 
     public function index(Request $request)
     {
-        $pageSize = 2;
+        $pageSize = 10;
         $page = (request()->has('page')) ? request('page') : 1;
 
         $query = Training::query()->skip($page * $pageSize - $pageSize)->limit($pageSize)
