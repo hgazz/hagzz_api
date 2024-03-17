a4ec8eb407092381e377ed7a2ab1f35c9161512d
Genius-code <moh.aly8890@gmail.com>
2024-03-17 09:45:02 +0200

    update_join_api

--- database/seeders/JoinSeeder.php
+++ database/seeders/JoinSeeder.php
@@ -15,7 +15,7 @@ class JoinSeeder extends Seeder
     public function run(): void
     {
         $invoice = Invoice::create([
-            'user_id'=>13,
+            'user_id'=>20,
             'training_id'=>2,
             'order_number'=>23,
             'status'=>'paid',
