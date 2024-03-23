867bb0c997b700ee832f3333678cf30ffb8d4867
karimgouda <dev.karimgouda@gmail.com>
2024-03-23 12:46:33 +0200

    notification

--- app/Http/Controllers/Api/JoinController.php
+++ app/Http/Controllers/Api/JoinController.php
@@ -4,38 +4,69 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Traits\apiResponse;
+use App\Models\Academies;
 use App\Models\Join;
+use App\Models\Notification;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class JoinController extends Controller
 {
     use apiResponse;
     public function addJoin(Request $request)
     {
-        $validations = Validator::make($request->all(),[
-           'invoice_id'=>'required|exists:invoices,id',
-           'training_id'=>'required|exists:trainings,id',
-            'price'=>'required|numeric|min:0.01',
-        ]);
-        if ($validations->fails()){
-            return $this->apiResponse(400, trans('api.validation_error'), $validations->errors());
+        try {
+            DB::beginTransaction();
+            $validations = Validator::make($request->all(),[
+                'invoice_id'=>'required|exists:invoices,id',
+                'training_id'=>'required|exists:trainings,id',
+                'price'=>'required|numeric|min:0.01',
+            ]);
+            if ($validations->fails()){
+                return $this->apiResponse(400, trans('api.validation_error'), $validations->errors());
+            }
+            $joinsExist = Join::where([
+                ['user_id',auth()->id()],
+                ['training_id',$request->training_id],
+            ])->exists();
+            if ($joinsExist){
+                return  $this->apiResponse(400 , null,trans('api.home.join training already exists'));
+            }
+            $joins = Join::create([
+                'user_id'=> auth()->id(),
+                'invoice_id' => $request->invoice_id,
+                'training_id'=>$request->training_id,
+                'price'=>$request->price,
+            ]);
+
+            //notification to academy
+            Notification::create([
+                'id'=>Str::uuid(),
+                'type'=>'New Booking',
+               'notifiable_type'=> Academies::class,
+                'notifiable_id'=> $joins->training->academy_id,
+                'data'=> auth()->user()->name.' booked '.$joins->training->academy->commercial_name,
+            ]);
+
+            //notifications to user
+            Notification::create([
+                'id'=>Str::uuid(),
+                'type'=>'Booking Confirmed',
+                'notifiable_type'=> User::class,
+                'notifiable_id'=> auth()->id(),
+                'data'=>'  your booking with '.$joins->training->academy->commercial_name.' is confirmed',
+            ]);
+
+            DB::commit();
+            return $this->apiResponse(200,trans('api.home.joined as training successfully'),null , $joins);
+        }catch (\Exception $e) {
+            dd($e->getMessage());
         }
-        $joinsExist = Join::where([
-            ['user_id',auth()->id()],
-            ['training_id',$request->training_id],
-        ])->exists();
-        if ($joinsExist){
-            return  $this->apiResponse(400 , null,trans('api.home.join training already exists'));
-        }
-       $joins = Join::create([
-            'user_id'=> auth()->id(),
-            'invoice_id' => $request->invoice_id,
-            'training_id'=>$request->training_id,
-            'price'=>$request->price,
-        ]);
-        return $this->apiResponse(200,trans('api.home.joined as training successfully'),null , $joins);
+
     }
 
     public function join(Request $request)
