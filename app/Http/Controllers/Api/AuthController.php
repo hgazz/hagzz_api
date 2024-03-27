6732f2732b24590de3861ed270f2b137252ba1ba
Genius-code <moh.aly8890@gmail.com>
2024-03-27 11:13:25 +0200

    update_notifications

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -99,7 +99,6 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        dd($request->all());
         $otp = rand(10000,99999);
         $validation = Validator::make($request->all(),[
             'phone'=> 'required|exists:users,phone',
