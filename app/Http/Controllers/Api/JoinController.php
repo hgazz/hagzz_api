553e1080d6a79beeff1911a507b2df3e57b93c15
Genuis-code <moh.aly8890@gmail.com>
2024-04-17 12:20:18 +0200

    update_join

--- app/Http/Controllers/Api/JoinController.php
+++ app/Http/Controllers/Api/JoinController.php
@@ -111,7 +111,7 @@ class JoinController extends Controller
         })->with([
             'training' => function ($query) {
                 $query->where('active', true);
-                $query->select(['id', 'name', 'image', 'price', 'start_date', 'end_date', 'max_players', 'level', 'gender', 'age_group', 'address_id', 'academy_id', 'active']);
+                $query->select(['id', 'name', 'price', 'start_date', 'end_date', 'max_players', 'level', 'gender', 'age_group', 'address_id', 'academy_id', 'active']);
                 $query->withCount(['joins', 'classes']);
             },
             'training.academy' => function ($query) {
