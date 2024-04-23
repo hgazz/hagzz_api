c688945f96b545dbfe0953709779b90b7edc8298
Genuis-code <moh.aly8890@gmail.com>
2024-04-23 16:33:43 +0200

    notifications_api

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -23,8 +23,8 @@ class Country extends Model
         return $this->hasMany(City::class);
     }
 
-    public function getNameAttribute()
+    protected function getNameAttribute()
     {
-        return $this->getTranslations('name')['ar'];
+        return $this->getTranslations('name')[$this->getLocale()];
     }
 }
