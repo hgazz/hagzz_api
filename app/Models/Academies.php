01732f6fb6a2390def9460917fed653da52d7f3d
Genuis-code <moh.aly8890@gmail.com>
2024-04-23 09:34:16 +0200

    notifications_api

--- app/Models/Academies.php
+++ app/Models/Academies.php
@@ -70,7 +70,7 @@ class Academies extends Model
         return $this->hasMany(Gallery::class, 'academy_id');
     }
 
-    protected function getCommercialNameAttribute()
+    public function getCommercialNameAttribute()
     {
         return $this->getTranslations('commercial_name')[$this->getLocale()];
     }
