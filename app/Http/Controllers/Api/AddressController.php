c37fba3839b530b9ab599cc0f45dbb93ed2483f8
Genuis-code <moh.aly8890@gmail.com>
2024-04-23 16:43:44 +0200

    notifications_api

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -16,7 +16,7 @@ class AddressController extends Controller
 
     public function getCountries()
     {
-        $countries = Country::first()->name;
+        $countries = Country::first();
         return $this->apiResponse(200, trans('api.countries.countries'), null, $countries);
     }
 
