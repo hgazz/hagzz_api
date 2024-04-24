f35efd03f33de0e9b95a23765b9f2cfda2a97800
gouda <dev.mohamedgouda@gmail.com>
2024-04-24 15:38:57 +0200

    academy response in home

--- app/Models/Academies.php
+++ app/Models/Academies.php
@@ -72,7 +72,7 @@ class Academies extends Model
 
     public function getCommercialNameAttribute()
     {
-        return (!empty($value)) ? json_decode($value)->{\App::getLocale()} : '';
+        return (!empty($value)) ? json_decode($value)->{$this->getLocale()} : '';
 
 //        dump($this->getTranslations('commercial_name')[$this->getLocale()]);
 //        return $this->getTranslations('commercial_name',['ar'])['ar'];
