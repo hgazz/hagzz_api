50e542a1c451795fccd87fe9206786a1c34e08df
Genuis-code <moh.aly8890@gmail.com>
2024-06-01 10:44:13 +0300

    update_academies_api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
     public function coachSportByUserFavSports()
     {
         // Get the sports IDs associated with the authenticated user
-        $userSportsIds = auth('api')->user()->sports->pluck('sport_id');
+        $userSportsIds = auth('api')->user()->sports->pluck('id');
         dd($userSportsIds);
         $coaches = CoachSport::whereIn('sport_id', $userSportsIds)
             ->with(['coach' => function ($query) {
