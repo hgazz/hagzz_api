7d7fa48cf32059c2c8a0f36aca9b033afb24e575
Genuis-code <moh.aly8890@gmail.com>
2024-07-13 10:39:33 +0300

    update_academies_api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -115,7 +115,8 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $otp = $request->phone == '01070809633' ? '12345' : rand(10000,99999);
+//        $otp = $request->phone == '01070809633' ? '12345' : rand(10000,99999);
+        $otp =  rand(10000,99999);
         $validation = Validator::make($request->all(),[
             'phone'=> 'required|exists:users,phone',
             'country_code' => 'required'
