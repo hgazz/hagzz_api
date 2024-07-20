398b51f11d7eb3344db3adb7eb38e80649801da1
Genuis-code <moh.aly8890@gmail.com>
2024-07-20 15:20:46 +0300

    update_academies_api

--- app/Http/Controllers/Api/TrainingController.php
+++ app/Http/Controllers/Api/TrainingController.php
@@ -94,7 +94,7 @@ class TrainingController extends Controller
 
             $request->whenHas('near_me', function () use($query){
                 $query->whereHas('address', function($query){
-                    return $query->where('country_id_id', auth('api')->user()->country_id);
+                    return $query->where('country_id', auth('api')->user()->country_id);
                 });
             });
 
