7b051b4d78c9b572a86cee411132cbedd9506f82
gouda <dev.mohamedgouda@gmail.com>
2024-09-03 14:59:16 +0300

    fix issue in fcm token

--- app/Services/Firebase/NotificationService.php
+++ app/Services/Firebase/NotificationService.php
@@ -33,7 +33,16 @@ class NotificationService
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
         curl_exec($ch);
+
+        if ($response === false || $httpCode !== 200) {
+            dd($response);
+            // Log the error or return an error response
+            return false;
+        }
 
         return true;
     }
