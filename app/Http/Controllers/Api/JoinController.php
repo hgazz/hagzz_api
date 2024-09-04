2da5efb0bb7054ffd9424596acb104b28c82f803
Genuis-code <moh.aly8890@gmail.com>
2024-09-04 10:53:50 +0300

    update_display_trainings

--- app/Http/Controllers/Api/JoinController.php
+++ app/Http/Controllers/Api/JoinController.php
@@ -99,7 +99,7 @@ class JoinController extends Controller
                 'image' => $join->training->academy->image,
                 'details' => $details
             ];
-            NotificationService::firebaseNotification($data, auth()->user()->fcm_token);
+            NotificationService::firebaseNotification($data, auth('api')->user()->fcm_token);
             NotificationService::dbNotification($join->user_id,User::class, 2, $title, $body, $join->training->academy->image, $details);
             $this->smsService->sendMessage($join->user->phone, "{$title} - {$body}");
             DB::commit();
