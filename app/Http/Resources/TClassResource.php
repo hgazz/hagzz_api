572b7de46909da323345e727a0ca45406233d68c
Genuis-code <moh.aly8890@gmail.com>
2024-09-09 14:54:29 +0300

    make_setting_endpoint

--- app/Http/Resources/TClassResource.php
+++ app/Http/Resources/TClassResource.php
@@ -22,7 +22,7 @@ class TClassResource extends JsonResource
             'start_time' => $this->start_time,
             'end_time' => $this->end_time,
             'out_comes' => $this->out_comes->en,
-            'bring_with_me' => $this->bring_with_me,
+            'bring_with_me' => $this->bring_with_me->app()->getLocale(),
         ];
     }
 }
