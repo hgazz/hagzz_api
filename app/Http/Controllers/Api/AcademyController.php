2b65585daa34b6b2704e9686ff858323438b5e07
Genuis-code <moh.aly8890@gmail.com>
2024-09-10 11:04:59 +0300

    coach_endpoint

--- app/Http/Controllers/Api/AcademyController.php
+++ app/Http/Controllers/Api/AcademyController.php
@@ -111,7 +111,7 @@ class AcademyController extends Controller
     private function buildTrainingsQuery($academyId)
     {
         return Training::where('academy_id', $academyId)
-            ->with(['academy', 'address', 'sport'])
+            ->with(['academy', 'address', 'sport', 'coach'])
             ->withCount(['joins', 'classes'])
             ->whereDate('start_date', '>=', Carbon::today()->toDateString())
             ->isActive();
