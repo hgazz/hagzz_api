0c559db7227e1164a862238ed45a8d2306c57167
Genuis-code <moh.aly8890@gmail.com>
2024-09-14 11:51:15 +0300

    update_setting

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\FaqResource;
 use App\Http\Resources\PartnerResource;
-use App\Http\Resources\SettingResource;
 use App\Http\Resources\SportResource;
 use App\Http\Resources\TrainingResource;
 use App\Http\Traits\apiResponse;
@@ -116,28 +115,12 @@ class HomeController extends Controller
     public function getSetting()
     {
         try {
-            $settingsGrouped = Setting::whereIn('key', ['egypt_address', 'qatar_address', 'email', 'whatsapp'])
-                ->get()
-                ->groupBy('key');
-
-            // Transform the keys after grouping
-            $settingsTransformed = $settingsGrouped->mapWithKeys(function ($item, $key) {
-                if ($key === 'egypt_address') {
-                    $key = 'Egypt Address';
-                }
-
-                if ($key === 'qatar_address') {
-                    $key = 'Qatar Address';
-                }
-                return [$key => $item];
-            });
-
-        } catch (\Exception $e) {
+            $settingsGrouped = Setting::whereIn('key', ['egypt_address','qatar_address', 'email', 'whatsapp'])->get()->groupBy('key');
+        }catch (\Exception $e){
             return $this->apiResponse(500, trans('api.something_went_wrong'), ['error' => $e->getMessage()]);
         }
 
-        // Using SettingResource to return the API response
-        return $this->apiResponse(200, trans('api.home.setting'), null, $settingsTransformed);
+        return $this->apiResponse(200, trans('api.home.setting'), null, $settingsGrouped);
     }
 
     public function getFaqs()
