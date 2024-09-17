a65ff25d2023bb3a8316dcce901acfa31fd3c905
Genuis-code <moh.aly8890@gmail.com>
2024-09-17 12:00:10 +0300

    update_setting

--- app/Http/Controllers/Api/CoachController.php
+++ app/Http/Controllers/Api/CoachController.php
@@ -57,7 +57,7 @@ class CoachController extends Controller
             $today = Carbon::now()->startOfDay();
 
             $upcomingTrainings = $trainings->filter(function ($training) use ($today) {
-                return $training->where([['start_date', '>=', now()], ['end_date', '>', $today]]);
+                return $training->where([['start_date', '>', $today], ['end_date', '>=', $today]]);
             })->values()->all(); // Reset keys and convert to array
 
             $pastTrainings = $trainings->filter(function ($training) use ($today) {
