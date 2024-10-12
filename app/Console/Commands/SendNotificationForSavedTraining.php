3a7d371012636abd2090f5963bb5e2f2fad7139a
Genuis-code <moh.aly8890@gmail.com>
2024-10-12 10:13:49 +0300

    update_notifications

--- app/Console/Commands/SendNotificationForSavedTraining.php
+++ app/Console/Commands/SendNotificationForSavedTraining.php
@@ -51,7 +51,6 @@ class SendNotificationForSavedTraining extends Command
 
         foreach ($joins as $join) {
             $training = $trainings->get($join->training_id);
-            $user = $join->user;
             if (!$training) continue; // Skip if training data is missing
 
             $joinsCount = Join::where('training_id', $join->training_id)->count();
@@ -79,8 +78,9 @@ class SendNotificationForSavedTraining extends Command
                     'latitude' => $training->address->latitude
                 ];
 
-                NotificationService::firebaseNotification($data, $user->fcm_token);
+                NotificationService::firebaseNotification($data, $join->user->fcm_token);
 //                NotificationService::dbNotification($user->id, User::class, 2, $title, $body, $training->academy->logo, $detail);
+                $this->info("Notification sent to user {$join->user->id}");
             }
         }
     }
